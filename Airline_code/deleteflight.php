a6d4aa7eeb93c29fccaf2ab3d4e232db000fda58
Ranindu Jaysooriya <43095375+Dinushan96@users.noreply.github.com>
2019-07-07 00:43:10 +0530

    Update deleteflight.php
    add button

--- Airline_code/deleteflight.php
+++ Airline_code/deleteflight.php
@@ -6,21 +6,15 @@
     <?php 
         include_once ('navbar.php');
     ?>
-
 <form action = "delete.php" method="post">
-
     <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
     <h1 class="w3-margin w3-xlarge">FLIGHT CANCELLATION</h1>
     </div>
-
     <div> User NIC </div>
     <div style="text-align:left">
     <input type="text", name="uid">
     </div>
-
-
     <li class="w3-container w3-black w3-center w3-opacity w3-padding-40">
     <input type="submit" value="Cancel Flight", name="btn">
     </li>
-
 </form>
